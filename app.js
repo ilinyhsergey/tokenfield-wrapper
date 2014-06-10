2f66de2963b69ae666d4867429ea03303f11509d
sergeyi <sergeyi@speaktoit.ru>
2014-06-10 16:00:19 +0700

    обёртка заготовка

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-$('#tokenfield-1').tokenfield({
-    autocomplete: {
-        source: ['red','blue','green','yellow','violet','brown','purple','black','white'],
-        delay: 100
-    },
-    showAutocompleteOnFocus: true,
-    tokens: ['black','white'],
-    createTokensOnBlur: true
-});
+(function () {
+
+    $('#tokenfield-1').tokenfield({
+        autocomplete: {
+            source: ['red', 'blue', 'green', 'yellow', 'violet', 'brown', 'purple', 'black', 'white'],
+            delay: 100
+        },
+        showAutocompleteOnFocus: true,
+        tokens: ['black', 'white'],
+        createTokensOnBlur: true
+    });
+
+    var app = angular.module("App", []);
+    app.directive('tokenfield', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var id = attrs.id;
+                var trimmer = function (e) {
+                    return e.trim();
+                };
+                var value = attrs.value.split(',').map(trimmer);
+                var autotokens = attrs.autotokens.split(',').map(trimmer);
+
+                $('#' + id).tokenfield({
+                    autocomplete: {
+                        source: autotokens,
+                        delay: 100
+                    },
+                    showAutocompleteOnFocus: true,
+                    tokens: value,
+                    createTokensOnBlur: true
+                });
+
+            }
+        }
+    });
+
+})();
