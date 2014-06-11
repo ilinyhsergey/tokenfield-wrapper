03d59ea3773fc9a1048fd3ec4851c515cbeca261
sergeyi <sergeyi@speaktoit.ru>
2014-06-11 16:38:29 +0700

    Hi is alive!

--- app.js
+++ app.js
@@ -11,27 +11,45 @@
     });
 
     var app = angular.module("App", []);
+
+    app.controller('AppCtrl', function ($scope) {
+        $scope.selectedtags = "white,pink";
+    });
+
+
     app.directive('tokenfield', function () {
         return {
             restrict: 'A',
-            link: function (scope, element, attrs) {
-                var id = attrs.id;
+            require: 'ngModel',
+            link: function (scope, element, attrs, ngModel) {
+
+                scope.$watch(
+                    function () {
+                        return ngModel.$modelValue;
+                    },
+                    function (newVal, oldVal) {
+                        if (newVal.trim().charAt(newVal.length - 1) != ',')
+                            element.tokenfield('setTokens', newVal);
+                    });
+
                 var trimmer = function (e) {
                     return e.trim();
                 };
+                // get tokens from value string
                 var value = attrs.value.split(',').map(trimmer);
+                // get array of autocomplete tokens as array
                 var autotokens = attrs.autotokens.split(',').map(trimmer);
 
-                $('#' + id).tokenfield({
-                    autocomplete: {
-                        source: autotokens,
-                        delay: 100
-                    },
-                    showAutocompleteOnFocus: true,
-                    tokens: value,
-                    createTokensOnBlur: true
-                });
-
+                element
+                    .tokenfield({
+                        autocomplete: {
+                            source: autotokens,
+                            delay: 100
+                        },
+                        showAutocompleteOnFocus: true,
+                        tokens: value,
+                        createTokensOnBlur: true
+                    });
             }
         }
     });
