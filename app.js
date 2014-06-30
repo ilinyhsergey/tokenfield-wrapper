def1f241f5ed30f4c5d0b781db6a79ec8399d8a0
Sergey Ilinykh <sergeyi@speaktoit.com>
2014-06-30 15:37:13 +0700

    'tokenfield' uses array as model

--- app.js
+++ app.js
@@ -4,10 +4,9 @@
     var app = ng.module("App", []);
 
     app.controller('AppCtrl', function ($scope) {
-        $scope.selectedtags = "Mr. White, J. Pinkman";
-//        $scope.selectedtags = ['Mr. White', 'J. Pinkman'];
-//        $scope.autocompletetokens = "!!!, red, blue, green, yellow, violet, brown, purple, black, white";
+        $scope.selectedtags = ['Mr.', 'White', 'J.', 'Pinkman'];
         $scope.autocompletetokens = ['!!!', 'red', 'blue', 'green', 'yellow', 'violet', 'brown', 'purple', 'black', 'white'];
+
     });
 
 
@@ -18,12 +17,23 @@
             scope: {
                 autotokens: "="
             },
-            link: function (scope, element, attrs, ngModel) {
+            link: function (scope, element, attrs, ngModelCtrl) {
+
+                ngModelCtrl.$parsers.push(function (viewValue) {
+                    if (ng.isString(viewValue)) viewValue = viewValue.split(',');
+
+                    return viewValue.map(function (e) {
+                        return e.trim();
+                    }).filter(function (e) {
+                        return !!e;
+                    });
+                });
+
 
                 // when the model changes, the tokens are added
                 scope.$watch(
                     function () {
-                        return ngModel.$modelValue;
+                        return ngModelCtrl.$modelValue;
                     },
                     function (newVal, oldVal) {
                         if (newVal != oldVal) {
@@ -37,36 +47,41 @@
                         return scope.autotokens;
                     },
                     function (newVal, oldVal) {
-                        if (newVal != oldVal) {
-                            element.tokenfield('destroy');
-                            init(ngModel.$modelValue);
-                        }
-                    });
+                        var autotokens = newVal || [];
 
-                // init function prepare config and initialise tokenfield
-                function init(tokensStr) {
-                    // get array of autocomplete tokens
-                    var autocompleteArr = (scope.autotokens || [])
-                        .map(function (e) {
+                        if (ng.isArray(autotokens)) {
+                            // do not nothing
+                        } else if (ng.isString(autotokens)) {
+                            // parse string
+                            autotokens = autotokens.split(',');
+                        } else if (ng.isObject(autotokens)) {
+                            // parse object
+                            var res = [];
+                            ng.forEach(autotokens, function (value, key) {
+                                if (value) res.push(key);
+                            });
+                            autotokens = res;
+                        } else {
+                            throw 'Not appropriate autocomplete source for "tokenfield"';
+                        }
+
+                        var autocompleteSource = autotokens.map(function (e) {
                             return e.trim();
-                        })
-                        .filter(function (e) {
+                        }).filter(function (e) {
                             return !!e;
                         });
 
-                    element.tokenfield({
-                        autocomplete: {
-                            source: autocompleteArr
-                        },
-                        showAutocompleteOnFocus: true,
-                        // get tokens from value string
-                        tokens: tokensStr || ngModel.$modelValue,
-                        createTokensOnBlur: true
+                        element.tokenfield('destroy');
+                        element.tokenfield({
+                            autocomplete: {
+                                source: autocompleteSource
+                            },
+                            showAutocompleteOnFocus: true,
+                            tokens: ngModelCtrl.$modelValue || [],
+                            createTokensOnBlur: true
+                        });
                     });
-                }
 
-                // init jQuery control 'tokenfield'
-                init();
             }
         }
     });
